74f143bacad3ffcb993726a5dc586ea972ac7e2f
Kenny Do <kedo@render.com>
2025-02-13 19:07:06 -0800

    add dummy torch endpoint

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import torch
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/torchrand')
+def torch_rand():
+    return str(torch.rand(5, 3))
+
